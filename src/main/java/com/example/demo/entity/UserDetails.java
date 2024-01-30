d54ffe020ca9d18c32a27ba9394369a69e8187b4
AnjaneshwarRao <89423692+AnjaneshwarRao@users.noreply.github.com>
2024-01-29 21:35:39 -0600

    Learnt how to create a custom methods in the repository class and how to use

---
+++ src/main/java/com/example/demo/entity/UserDetails.java
@@ -0,0 +1,94 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "UserDetails")
+public class UserDetails {
+	@Id
+	private String emailId;
+	@Column(name = "password")
+	private String password;
+	@Column(name = "name")
+	private String name;
+	@Column(name = "gender")
+	private String gender;
+	@Column(name = "contact")
+	private long contact;
+	@Column(name = "city")
+	private String city;
+	@Column(name = "state")
+	private String state;
+	@Column(name = "country")
+	private String country;
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getContact() {
+		return contact;
+	}
+
+	public void setContact(long contact) {
+		this.contact = contact;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+}
---
+++ src/main/java/com/example/demo/service/UserDetailsService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserDetailsService {
+	
+	@Autowired
+	User
+
+}
